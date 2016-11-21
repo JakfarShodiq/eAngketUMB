2bbf744d3dbde23c8aa81c53601d2416bcf6d58c
ramdh09 <fajar.ramdh09@gmail.com>
2016-11-21 17:05:48 +0700

    fix Register page

--- app/Roles.php
+++ app/Roles.php
@@ -3,10 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Roles extends Model
 {
-    //
-    protected $fillable = ['name','status'];
+    use SoftDeletes;
 
+    //
+    protected $table = 'roles';
+    protected $fillable = ['name','status'];
+    protected $dates = ['deleted_at'];
 }
